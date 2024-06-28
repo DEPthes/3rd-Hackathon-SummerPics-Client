064163a56697bccdebdedcab893cf831dd84b8bb
si14444 <a1@1ui-MacBookAir.local>
2024-06-29 00:55:18 +0900

    feat: mainPage

--- src/components/main/ViewWeather.jsx
+++ src/components/main/ViewWeather.jsx
@@ -12,11 +12,9 @@ const ViewWeather = () => {
   return (
     <main className="mainComponent">
       <h1 className="viewFont">
-        {dummyWeatherData.location}의 <br />
-        {dummyWeatherData.day}은 현재...
+        ♣{dummyWeatherData.location}♣의 <br />★{dummyWeatherData.day}★은 현재...
       </h1>
-      <div class="circle"></div>
-      <h1 className="tempFont">{dummyWeatherData.temperature}℃</h1>
+      <h1 className="tempFont">~ {dummyWeatherData.temperature}℃ ~</h1>
       <p className="statusMessage">{dummyWeatherData.statusMessage}</p>
       <Link to="/recommend" className="recomendBtton">
         오늘의 짤 추천받기
